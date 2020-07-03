20e046932455dc3480e5367e1e937b13d5df83fb
aleks93621 <aleks936@hotmail.fr>
2020-07-04 00:01:46 +0200

    ok

--- level0/test.c
+++ level0/test.c
@@ -4,26 +4,25 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int ac, char **av) {
+int	main(int ac, char **av)
+{
     gid_t	egid;
     uid_t	euid;
     int		nbr;
     char	**cmd;
 
-    nbr = atoi(av[1]);
-    if (nbr == 423)
+	nbr = atoi(av[1]);
+	if (nbr == 423)
 	{
-        cmd[0] = strdup("/bin/sh");
+		cmd[0] = strdup("/bin/sh");
 		cmd[1] = 0;
-        egid = getegid();
-        euid = geteuid();
-
+		egid = getegid();
+		euid = geteuid();
 		setresgid(egid, egid, egid);
 		setresuid(euid, euid, euid);
-
 		execv("/bin/sh", cmd);
-    }
+	}
 	else
-        fwrite("No !\n", 1, 5, stderr);
+		fwrite("No !\n", 1, 5, stderr);
 	return 0;
 }
